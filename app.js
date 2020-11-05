9aae9ed9554a70d5e8464c348c54dc8cc21fdcf4
lhutchinson22 <lbhutchinson022@gmail.com>
2020-11-04 20:32:09 -0800

    accessed api

--- app.js
+++ app.js
@@ -16,13 +16,16 @@ $("document").ready(function () {
     console.log(citySearchArray);
 
     for (var i = 0; i < citySearchArray.length; i++) {
+      //   var createInputDiv = document.createElement("div");
+      // createInputDiv.textContent = citySearchArray[i]
+
       $("#search-history").append(citySearchArray[i] + " " + "<br>");
     }
 
     // fetch request
     var apiKey = "89e5c40c6fdcef655f97ee07fb64e4b1";
     var requestUrl =
-      "api.openweathermap.org/data/2.5/weather?q=" +
+      "http://api.openweathermap.org/data/2.5/weather?q=" +
       citySearchText +
       "&appid=" +
       apiKey;
@@ -34,24 +37,6 @@ $("document").ready(function () {
       })
       .then(function (data) {
         console.log(data);
-        //Loop over the data to generate a table, each table row will have a link to the repo url
-        //   for (var i = 0; i < data.length; i++) {
-        //     // Creating elements, tablerow, tabledata, and anchor
-        //     var createTableRow = document.createElement("tr");
-        //     var tableData = document.createElement("td");
-        //     var link = document.createElement("a");
-
-        // Setting the text of link and the href of the link
-        //   link.textContent = data[i].html_url;
-        //   link.href = data[i].html_url;
-
-        // Appending the link to the tabledata and then appending the tabledata to the tablerow
-        // The tablerow then gets appended to the tablebody
-        //   tableData.appendChild(link);
-        //   createTableRow.appendChild(tableData);
-        //   tableBody.appendChild(createTableRow);
-        //   }
       });
   });
 });
-// fetchButton.addEventListener("click", getApi);
