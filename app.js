af61cbdb12c6bddf53fbb191c594c322973e0759
lhutchinson22 <lbhutchinson022@gmail.com>
2020-11-05 09:10:21 -0800

    retrieved forecast on search

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 console.log("sanity check");
 
 $("document").ready(function () {
+  // moment.js
+  //   var now = moment().format("LLLL");
+  //   console.log(now);
+  //   currentDay.append(now);
+
   var fetchButton = document.getElementById("fetch-button");
   var cityInput = document.getElementById("search-input-text");
+  var currentForecast = document.getElementById("main-search-output");
+
   $("#fetch-button").on("click", function (event) {
     event.preventDefault();
     console.log("search button is clicked");
@@ -37,6 +44,35 @@ $("document").ready(function () {
       })
       .then(function (data) {
         console.log(data);
+
+        var latitude = data.coord.lat;
+        var longitude = data.coord.lon;
+
+        uvIndex(latitude, longitude);
+
+        $("#temp").text("TEMP IN K: " + data.main.temp);
+        $("#humidity").text("HUMIDITY: " + data.main.humidity);
+        $("#windSpeed").text("WIND SPEED: " + data.wind.speed);
       });
   });
+
+  function uvIndex(lat, lon) {
+    var apiKey = "89e5c40c6fdcef655f97ee07fb64e4b1";
+    var requestUrl =
+      "http://api.openweathermap.org/data/2.5/uvi?lat=" +
+      lat +
+      "&lon=" +
+      lon +
+      "&appid=" +
+      apiKey;
+
+    fetch(requestUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+        $("#uvIndex").text("UV INDEX: " + data.value);
+      });
+  }
 });
